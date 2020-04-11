0a88c2d494fd2790feac48f4e7552b7fec31d9a3
Jordan Hanson <JORDANMERCEDES22@GMAIL.COM>
2020-04-11 14:13:29 -0600

    jaws db update

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 const mysql = require("mysql");
+let connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: process.env.MYSQL_PASSWORD,
-    database: "burgers_db"
-});
 
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: process.env.MYSQL_PASSWORD,
+        database: "burgers_db"
+    });
+}
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id" + connection.threadId);
